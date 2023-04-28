e241a8d549dd4341905c38ad4e90ae59b40f5fc4
ElizaManthei <mazilemka@gmail.com>
2023-04-28 10:08:53 +0200

    main

---
+++ src/main/java/nl/jkspecs/JkSpecsApplication.java
@@ -0,0 +1,13 @@
+package nl.jkspecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkSpecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkSpecsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/jkspecs/JkSpecsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jkspecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkSpecsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
